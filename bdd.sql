cf1c2bc528f187687ebd42b4e6860622ca050a8d
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2025-03-12 10:16:11 +0100

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE bmi_calculator;
+USE bmi_calculator;
 
+CREATE TABLE bmi_records (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    weight FLOAT NOT NULL,
+    height FLOAT NOT NULL,
+    bmi FLOAT NOT NULL,
+    interpretation VARCHAR(50) NOT NULL
+);
