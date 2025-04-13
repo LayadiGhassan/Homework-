9c600c0f65f556cdbfc8f5a08641e7fab0326654
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2025-04-13 17:06:59 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,16 +7,16 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 require 'config/database.php';
-require 'app/models/BmiModel.php';
-require 'app/controllers/BmiController.php';
+require 'models/BmiModel.php';
+require 'controllers/BmiController.php';
 
 $model = new BmiModel($conn);
 $controller = new BmiController($model);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $controller->calculateBmi($_SESSION['user_id'], $_POST['name'], $_POST['weight'], $_POST['height']);
-    require 'app/views/bmi_result.php';
+    require 'views/bmi_result.php';
 } else {
-    require 'app/views/bmi_form.php';
+    require 'views/bmi_form.php';
 }
 ?>
