8a191f64e6ac2e9ff17f30fe0576d130a138241e
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2025-04-13 16:32:22 +0100

    Create bdd.php

---
+++ bdd.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db = 'bmi_calculator';
+$user = 'root'; 
+$pass = ''; 
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
